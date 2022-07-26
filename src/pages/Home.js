b7cfc4162e75b4cb5c810a6b4e9c714664807101
Shadabimran07 <md.shadabimran.aiml25@heritageit.edu.in>
2022-07-27 00:10:59 +0530

    learn How to fetch result and data on enter key press

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
 
-export const Home = () => <MainPageLayout>This is Home Page</MainPageLayout>;
+// export const Home = () => (
+//     const [input, setInput] = useState('');
+//     const onInputChange = ev => setInput(ev.target.value);
+//   <MainPageLayout>
+//     <input type="text" onChange={onInputChange} />
+//   </MainPageLayout>
+// );
+
+const Home = () => {
+  const [input, setInput] = useState('');
+  const onInputChange = ev => setInput(ev.target.value);
+  const onSearch = () => {
+    fetch(`https://api.tvmaze.com/singlesearch/shows?q=${input}`)
+      .then(r => r.json())
+      .then(result => {
+        console.log(result);
+      });
+  };
+  const onKeyDown = ev => {
+    if (ev.keyCode === 13) {
+      onSearch();
+    }
+  };
+  return (
+    <MainPageLayout>
+      <input
+        type="text"
+        onChange={onInputChange}
+        onKeyDown={onKeyDown}
+        value={input}
+      />
+      <button type="button" onClick={onSearch}>
+        Search
+      </button>
+    </MainPageLayout>
+  );
+};
+export default Home;
