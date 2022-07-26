cc1936a0032aa623c8bc7405beb157e5182f4443
Shadabimran07 <md.shadabimran.aiml25@heritageit.edu.in>
2022-07-27 01:08:57 +0530

    api done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,18 +11,38 @@ import MainPageLayout from '../components/MainPageLayout';
 
 const Home = () => {
   const [input, setInput] = useState('');
-  const onInputChange = ev => setInput(ev.target.value);
+  const [results, setResults] = useState(null);
+
   const onSearch = () => {
     fetch(`https://api.tvmaze.com/singlesearch/shows?q=${input}`)
       .then(r => r.json())
       .then(result => {
+        setResults(result);
         console.log(result);
       });
   };
+  const onInputChange = ev => setInput(ev.target.value);
   const onKeyDown = ev => {
     if (ev.keyCode === 13) {
       onSearch();
     }
+  };
+  const renderResults = () => {
+    if (results && results.length === 0) {
+      return <div>No results</div>;
+    }
+
+    if (results && results.length > 0) {
+      return (
+        <div>
+          {results.map(item => (
+            <div key={item.show.id}>{item.show.name}</div>
+          ))}
+        </div>
+      );
+    }
+
+    return null;
   };
   return (
     <MainPageLayout>
@@ -35,6 +55,7 @@ const Home = () => {
       <button type="button" onClick={onSearch}>
         Search
       </button>
+      {renderResults()}
     </MainPageLayout>
   );
 };
