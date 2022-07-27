e839d77dec4073c9e1e2a221819beedb28de73db
Shadabimran07 <md.shadabimran.aiml25@heritageit.edu.in>
2022-07-28 02:44:46 +0530

    styling of actor and show done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,55 +1,86 @@
 import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
 import { apiGet } from '../misc/config';
-
-// export const Home = () => (
-//     const [input, setInput] = useState('');
-//     const onInputChange = ev => setInput(ev.target.value);
-//   <MainPageLayout>
-//     <input type="text" onChange={onInputChange} />
-//   </MainPageLayout>
-// );
+import ShowGrid from '../components/show/ShowGrid';
+import ActorGrid from '../components/actor/ActorGrid';
 
 const Home = () => {
   const [input, setInput] = useState('');
   const [results, setResults] = useState(null);
+  const [searchOption, setSearchOption] = useState('shows');
+
+  const isShowsSearch = searchOption === 'shows';
 
   const onSearch = () => {
-    apiGet(`/search/shows?q=${input}`).then(result => {
+    apiGet(`/search/${searchOption}?q=${input}`).then(result => {
       setResults(result);
     });
   };
-  const onInputChange = ev => setInput(ev.target.value);
+
+  const onInputChange = ev => {
+    setInput(ev.target.value);
+  };
+
   const onKeyDown = ev => {
     if (ev.keyCode === 13) {
       onSearch();
     }
   };
+
+  const onRadioChange = ev => {
+    setSearchOption(ev.target.value);
+  };
+
   const renderResults = () => {
     if (results && results.length === 0) {
       return <div>No results</div>;
     }
 
     if (results && results.length > 0) {
-      return (
-        <div>
-          {results.map(item => (
-            <div key={item.show.id}>{item.show.name}</div>
-          ))}
-        </div>
+      return results[0].show ? (
+        <ShowGrid data={results} />
+      ) : (
+        <ActorGrid data={results} />
       );
     }
 
     return null;
   };
+
   return (
     <MainPageLayout>
       <input
         type="text"
+        placeholder="Search for something"
         onChange={onInputChange}
         onKeyDown={onKeyDown}
         value={input}
       />
+
+      <div>
+        <label htmlFor="shows-search">
+          Shows
+          <input
+            id="shows-search"
+            type="radio"
+            value="shows"
+            checked={isShowsSearch}
+            onChange={onRadioChange}
+          />
+        </label>
+
+        <label htmlFor="actors-search">
+          Actors
+          <input
+            id="actors-search"
+            type="radio"
+            value="people"
+            checked={!isShowsSearch}
+            onChange={onRadioChange}
+          />
+        </label>
+      </div>
+
       <button type="button" onClick={onSearch}>
         Search
       </button>
@@ -57,4 +88,5 @@ const Home = () => {
     </MainPageLayout>
   );
 };
+
 export default Home;
