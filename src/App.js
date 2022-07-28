94c2f16e85237dec1c83665ed5777588116b736d
Shadabimran07 <md.shadabimran.aiml25@heritageit.edu.in>
2022-07-29 00:08:40 +0530

    done reducer part

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Show from './pages/Show';
 
 import Home from './pages/Home';
 import Starred from './pages/Starred';
@@ -13,8 +14,8 @@ function App() {
       <Route exact path="/starred">
         <Starred />
       </Route>
-      <Route>
-        <div>Page Not Found !</div>
+      <Route exact path="/show/:id">
+        <Show />
       </Route>
     </Switch>
   );
