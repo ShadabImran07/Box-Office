3129187bdf489ae809143ae9d23190d153ff89e4
Shadabimran07 <md.shadabimran.aiml25@heritageit.edu.in>
2022-07-29 02:29:12 +0530

    Error Occured

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,5 +1,10 @@
+/* eslint-disable no-underscore-dangle */
 import React, { useEffect, useReducer } from 'react';
 import { useParams } from 'react-router-dom';
+import Cast from '../components/show/Cast';
+import Details from '../components/show/Details';
+import Seasons from '../components/show/Seasons';
+import ShowMainData from '../components/show/ShowMainData';
 import { apiGet } from '../misc/config';
 
 const reducer = (prevState, action) => {
@@ -59,6 +64,35 @@ const Show = () => {
   if (error) {
     return <div>Error occured : {error}</div>;
   }
-  return <div>This is Show Page</div>;
+  return (
+    <div>
+      <ShowMainData
+        image={show.image}
+        name={show.name}
+        rating={show.rating}
+        summary={show.summary}
+        tags={show.genres}
+      />
+
+      <div>
+        <h2>Details</h2>
+        <Details
+          status={show.status}
+          network={show.network}
+          premiered={show.premiered}
+        />
+      </div>
+
+      <div>
+        <h2>Seasons</h2>
+        <Seasons Seasons={show._embedded.Seasons} />
+      </div>
+
+      <div>
+        <h2>Cast</h2>
+        <Cast Seasons={show._embedded.cast} />
+      </div>
+    </div>
+  );
 };
 export default Show;
